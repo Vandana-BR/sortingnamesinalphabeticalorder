5547cac2633a8084fcb56f99be720cc4417302f9
Vandana B R <vandanabr97@gmail.com>
2017-10-15 12:27:27 +0530

    sorting names in alphabetical order

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    char name[20][20], tname[20][20], temp[20];
+    int i, j, n;
+
+    printf("Enter the value of n : \n");
+    scanf("%d", &n);
+    printf("Enter %d names:", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%s", name[i]);
+        strcpy(tname[i], name[i]);
+    }
+
+    printf("Entered names:");
+    for (i = 0; i < n; i++)
+    {
+        printf("%s ", name[i]);
+
+    }
+    for (i = 0; i < n - 1 ; i++)
+    {
+        for (j = i + 1; j < n; j++)
+        {
+            if (strcmp(name[i], name[j]) > 0)
+            {
+                strcpy(temp, name[i]);
+                strcpy(name[i], name[j]);
+                strcpy(name[j], temp);
+            }
+        }
+    }
+    printf("\n----------------------------------------\n");
+    printf("Input NamestSorted names\n");
+    printf("------------------------------------------\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%s\t%s\n", tname[i], name[i]);
+    }
+    printf("------------------------------------------\n");
+}
